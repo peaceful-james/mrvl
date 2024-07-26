4efd4a543a5bc87220de87d9a4d6a5bb84cb2fd3
James Gough <james@peacefulprogramming.dev>
2024-07-26 14:40:53 +0100

    improve modal patching logic

--- lib/mrvl_web/live/characters_live.ex
+++ lib/mrvl_web/live/characters_live.ex
@@ -8,21 +8,26 @@ defmodule MrvlWeb.CharactersLive do
   alias Mrvl.Marvel
 
   @impl LiveView
-  def mount(params, _session, socket) do
-    modal_character_id = Map.get(params, "marvel_id")
-
+  def mount(_params, _session, socket) do
     socket
     |> assign_characters()
-    |> assign(:modal_character_id, modal_character_id)
     |> then(&{:ok, &1})
   end
 
   @impl LiveView
   def handle_params(unsigned_params, _url, socket) do
-    modal_character_id = Map.get(unsigned_params, "marvel_id")
+    modal_character =
+      if socket.assigns.live_action == :character do
+        marvel_id_string = Map.get(unsigned_params, "marvel_id")
+
+        case Integer.parse(marvel_id_string) do
+          {modal_character_id, ""} -> Enum.find(socket.assigns.characters, &(&1.marvel_id == modal_character_id))
+          _ -> nil
+        end
+      end
 
     socket
-    |> assign(:modal_character_id, modal_character_id)
+    |> assign(:modal_character, modal_character)
     |> then(&{:noreply, &1})
   end
 
@@ -30,14 +35,14 @@ defmodule MrvlWeb.CharactersLive do
   def render(assigns) do
     ~H"""
     <div id="home-live" class="w-full px-32 grid gap-4 md:gap-6 grid-cols-2 md:grid-cols-4">
-      <.character :for={character <- @characters} character={character} />
+      <.character_logo :for={character <- @characters} character={character} />
       <.modal
-        :if={@modal_character_id}
+        :if={@modal_character}
         show={true}
-        id={"character-modal-#{@modal_character_id}"}
+        id={"character-modal-#{@modal_character.marvel_id}"}
         on_cancel={JS.patch(~p"/")}
       >
-        This is a modal.
+        <.character_details character={@modal_character} />
       </.modal>
     </div>
     """
@@ -45,7 +50,24 @@ defmodule MrvlWeb.CharactersLive do
 
   attr :character, Character, required: true
 
-  defp character(assigns) do
+  defp character_logo(assigns) do
+    ~H"""
+    <div
+      id={"character-#{@character.marvel_id}"}
+      class="relative rounded-md h-32 w-32 cursor-pointer hover:border-2"
+      phx-click={JS.patch(~p"/character/#{@character.marvel_id}")}
+    >
+      <img src={@character.thumbnail} class="object-cover h-32 w-32" />
+      <div class="absolute -bottom-0 w-full bg-black/60 text-white text-center">
+        <%= @character.name %>
+      </div>
+    </div>
+    """
+  end
+
+  attr :character, Character, required: true
+
+  defp character_details(assigns) do
     ~H"""
     <div
       id={"character-#{@character.marvel_id}"}
