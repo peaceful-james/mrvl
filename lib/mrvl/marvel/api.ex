28fba07ba271b77f9d2966c348d67aeb97f6690c
James Gough <james@peacefulprogramming.dev>
2024-07-26 12:00:20 +0100

    tidy the api module a bit

--- lib/mrvl/marvel/api.ex
+++ lib/mrvl/marvel/api.ex
@@ -15,22 +15,25 @@ defmodule Mrvl.Marvel.Api do
 
   @spec get_characters() :: term()
   def get_characters do
-    get("public/characters")
+    "public/characters"
+    |> path_with_auth()
+    |> get()
   end
 
-  defp build_ts, do: :microsecond |> NaiveDateTime.utc_now() |> NaiveDateTime.to_iso8601()
+  defp build_timestamp, do: :microsecond |> NaiveDateTime.utc_now() |> NaiveDateTime.to_iso8601()
 
   defp build_hash(to_hash) do
     to_hash |> :erlang.md5() |> Base.encode16(case: :lower)
   end
 
-  def build_ts_and_hash_params do
+  defp path_with_auth(base_path) do
     private_key = Application.get_env(:mrvl, :marvel_private_key)
     public_key = Application.get_env(:mrvl, :marvel_public_key)
 
-    ts = build_ts()
+    ts = build_timestamp()
     hash = build_hash(ts <> private_key <> public_key)
 
-    %{"ts" => ts, "hash" => hash}
+    encoded_query = URI.encode_query(%{"apikey" => public_key, "ts" => ts, "hash" => hash})
+    base_path |> URI.new!() |> URI.append_query(encoded_query) |> URI.to_string()
   end
 end
